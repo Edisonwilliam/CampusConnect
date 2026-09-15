82267378975c0f04e71c0fd213ed3a44654fd86f
Edisonwilliam <admiralcavani@gmail.com>
2026-09-15 01:20:03 +0100

    done backend

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -39,7 +39,7 @@ export class AuthController {
   async googleCallback(@Request() req: any, @Res() res: Response) {
     const result = await this.authService.googleLogin(req.user);
 
-    const frontendUrl = 'http://localhost:3000';
+    const frontendUrl = `${process.env.FRONTEND_URL}/auth/google/callback`;
 
     const params = new URLSearchParams({
       token: result.access_token,
