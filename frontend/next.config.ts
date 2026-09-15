46543393d9e3f60ecbffb781a13aee327cb88bd5
Edisonwilliam <admiralcavani@gmail.com>
2026-09-15 17:57:32 +0100

    Fix production marketplace images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,6 +11,12 @@ const nextConfig: NextConfig = {
         port: "5000",
         pathname: "/uploads/**",
       },
+
+       {
+        protocol: "https",
+        hostname: "campusconnect-qgah.onrender.com",
+        pathname: "/uploads/**",
+      },
     ],
   },
 };
